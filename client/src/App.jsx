2b6656e30680d41adcc9bfe9b4cdd9ee9f9535ed
emily <emilycassel77@gmail.com>
2024-07-22 13:30:09 -0400

    fixed css for home, resume, aboutme, projects

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,37 +1,33 @@
-import { Routes, Route } from 'react-router-dom'
-import { useState, useEffect } from 'react'
+import { Routes, Route } from 'react-router-dom';
+import { useState } from 'react';
 
-import {Header, Footer} from './components'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
-import {Home, Portfolio, Resume, AboutMe, Contact}  from './pages'
-
-
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
+import AboutMe from './pages/AboutMe';
+import Contact from './pages/Contact';
 
 function App() {
-  const [isDarkMode, setIsDarkMode] = useState(true)
-
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
   return (
-    <div>
-      <div className={`${isDarkMode ? 'dark' : ''}`}>
-        <Header isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
-        <main>
-
-          <Routes>
-            <Route path="/" element={<Home />} />
-            
-            <Route path="/Portfolio" element={<Portfolio />} />
-
-            <Route path="/AboutMe" element={<AboutMe />} />
-
-            <Route path="/Resume" element={<Resume />} />
-            <Route path="/Contact" element={<Contact />} />
-          </Routes>
-        </main>
-        <Footer/>
-      </div>
+    <div id="main-container" className={`${isDarkMode ? 'dark' : ''}`}>
+      <Header isDarkMode={isDarkMode} setIsDarkMode={setIsDarkMode} />
+      <main className="content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Portfolio" element={<Portfolio />} />
+          <Route path="/AboutMe" element={<AboutMe />} />
+          <Route path="/Resume" element={<Resume />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </main>
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,7 +3,7 @@ function Home() {
         <div className="homeIntro">
             <h1 className="introName">Emily Cassel</h1>
             <hr className="homeLine"></hr>
-                <h2 className="jobAm">Full Stack Web Development</h2>
+                <h2 className="jobAm">Full Stack Web Developer</h2>
             <hr className="homeLine"></hr>
             <div className="myHomeLinks">
                         <a className="link" href="https://www.linkedin.com/in/emily-cassel-5a2980245/">LinkedIn</a>
@@ -12,7 +12,6 @@ function Home() {
                     <hr className="homeLine"></hr>
                 <div className="introLines">
                     <p className="introSentence">I am a recent graduate of well-respected full stack web development from Rutgers University Coding Bootcamp program. I am looking for full time employment in this exciting and creative field.</p>
-                    
                     <p className="introSentence">I excel and specialize in JavaScript, CSS, and UI/UX design and development. I am adept at collaborating within agile environments, delivering clean code, and communicating effectively.</p>
                     <p className="introSentence">I pursued this program to enrich my technological capabilities while my Customs Brokers License was being processed. During my coding course I became completely engaged and recognized my passion for a career in web development.</p>
                 </div> 
