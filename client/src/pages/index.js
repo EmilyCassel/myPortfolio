61031519f9d241524b31da316bd925fa82553d1c
emily <emilycassel77@gmail.com>
2024-07-22 04:40:29 -0400

    index added

---
+++ client/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from './Home';
+export { default as Contact } from './Contact';
+export { default as Portfolio } from './Portfolio';
+export { default as Resume } from './Resume';
+export { default as AboutMe } from './AboutMe';
